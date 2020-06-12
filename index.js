7aabd9c62e0192fe3e231cac9e8775a3df6fd48a
Joshua Patterson <joshualpatterson@gmail.com>
2020-06-12 02:22:27 -0400

    Added github webhook auto deploy.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const childProcess = require('child_process')
+const GITHUB_WEBHOOK_SECRET = 'fD8xTzB6D42tAG4g432GQ#43g#$gq34g#Q%GQ35g3#%Gq2g365hQH'
+const GITHUB_USER = 'shnibble'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World v2'))
+
+app.post('/webhooks/github', (req, res) => {
+    const sender = req.body.sender
+    const branch = req.body.ref
+
+    if (branch.indexOf('master') > -1 && sender.login === GITHUB_USER) {
+        deploy(res)
+    }
+})
+
+const deploy = (res) => {
+    childProcess.exec('cd ~/scripts && ./deploy.sh', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            return res.send(500)
+        }
+        res.send(200)
+    })
+}
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`))
