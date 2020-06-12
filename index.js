17be5a49abbc6e2d1186d2c081601e118e05c6a1
Joshua Patterson <joshualpatterson@gmail.com>
2020-06-12 16:30:12 -0400

    Added environment variable support and removed plain text github webhook secret.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const childProcess = require('child_process')
-const GITHUB_WEBHOOK_SECRET = 'fD8xTzB6D42tAG4g432GQ#43g#$gq34g#Q%GQ35g3#%Gq2g365hQH'
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET
 const GithubWebHook = require('express-github-webhook')
 const webhookHandler = GithubWebHook({ path: '/webhooks/github', secret: GITHUB_WEBHOOK_SECRET })
 
