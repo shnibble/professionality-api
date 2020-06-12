b2e56fe92f70d1e2fc67c90ded0beb99659f2648
Joshua Patterson <joshualpatterson@gmail.com>
2020-06-12 13:57:27 -0400

    i

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const webhookHandler = GithubWebHook({ path: '/webhooks/github', secret: GITHUB_
 app.use(bodyParser.json())
 app.use(webhookHandler)
 
-app.get('/', (req, res) => res.send('Hello World v10'))
+app.get('/', (req, res) => res.send('Hello World v11'))
 
 webhookHandler.on('*', (event, repo, data) => {
     console.log('Incoming webhook event from Github.')
