10c5d9d5984a753524e466310a6b35f5b15b5a8b
Joshua Patterson <joshualpatterson@gmail.com>
2020-06-26 10:37:31 -0400

    Fixed up search query, removed console log.

--- controllers/items.js
+++ controllers/items.js
@@ -1,12 +1,12 @@
 const get = (req, res, connection) => {
     const search = req.query.search || ''
+    const fullSearch = '%' + search + '%'
 
-    connection.execute(`SELECT * FROM items WHERE name LIKE '%?%' ORDER BY name LIMIT 10`, [search], (err, results, fields) => {
+    connection.execute(`SELECT * FROM items WHERE name LIKE ? ORDER BY name LIMIT 20`, [fullSearch], (err, results, fields) => {
         if (err) {
             console.error(err)
             res.status(500).send('Server error')
         } else {
-            console.log('Results:', results)
             res.status(200).json(results)
         }
     })
