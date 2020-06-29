e1f763690682b5ffedc80ce324a4f37766eb594d
Joshua Patterson <joshualpatterson@gmail.com>
2020-06-29 09:59:32 -0400

    Added wowhead crawler to bank endpoint.

--- controllers/bank.js
+++ controllers/bank.js
@@ -1,4 +1,6 @@
 const JWT = require('../util/jwt')
+const parser = require('fast-xml-parser')
+const axios = require('axios')
 
 const getGoals = (req, res, connection) => {
     connection.query('SELECT * FROM bank_goals ORDER BY id', (err, results, fields) => {
@@ -158,16 +160,42 @@ const updateGoal = (req, res, connection) => {
      }
 }
 
+const getInventoryItemDetails = (item_id) => {
+    axios.get(`https://classic.wowhead.com/item=${item_id}&xml`)
+    .then(response => {
+        return(parser.parse(response.data))
+    })
+    .catch(err => {
+        console.error(err)
+        return false
+    })
+}
+
 const getInventory = (req, res, connection) => {
     connection.query('SELECT * FROM bank_inventory ORDER BY name', (err, results, fields) => {
         if (err) {
             console.error(err)
             res.status(500).send('Server error')
         } else {
-            res.status(200).json(results)
+
+            let n = 0
+            results.forEach(async row => {
+                const data = await getInventoryItemDetails(row.item_id)
+                results[n].name = data.wowhead.item.name || 'Item Not Found'
+                results[n].quality = data.wowhead.item.quality || 'Poor'
+                results[n].icon = data.wowhead.item.icon || 'classic_temp'
+
+                n++
+
+                if (n === results.length) {
+                    res.status(200).json(results)
+                }
+            })
         }
     })
 }
+
+
 
 const addInventory = (req, res, connection) => {
     // validate parameters
