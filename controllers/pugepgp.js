c2a8d04dd852ee58946ec232d793d0ad0af4c933
Joshua Patterson <joshualpatterson@gmail.com>
2020-08-31 10:15:36 -0400

    Added remaining pugepgp endpoints.

--- controllers/pugepgp.js
+++ controllers/pugepgp.js
@@ -1,6 +1,8 @@
 const JWT = require('../util/jwt')
+const connection = require('../db/connect')
 const MINIMUM_GP = 20
 const TRANSACTION_LIMIT = 20
+const DECAY_RATE = 0.2
 
 const get = (req, res, connection) => {
     connection.query('SELECT id, name, ep, gp, (ep/gp) as pr FROM pug_epgp WHERE active = TRUE ORDER BY name', (err, results, fields) => {
@@ -232,22 +234,227 @@ const updateActiveEp = (req, res, connection) => {
 
 const applyDecay = (req, res, connection) => {
 
+    // validate parameters
+    const { jwt } = req.body
+
+    if (typeof jwt === 'undefined') {
+        res.status(400).send('Bad request')
+    } else {
+
+        // verify jwt
+        JWT.verify(jwt)
+        .then(jwt_data => {
+
+            // confirm officer rank
+            if (!jwt_data.body.is_officer) {
+                res.status(403).send('Forbidden')
+            } else {
+                
+                // get all pugs
+                connection.query('SELECT * FROM pug_epgp', (err, results, fields) => {
+                    if (err) {
+                        console.error(err)
+                        res.status(500).send('Server error')
+                    } else {
+
+                        // iterate through pugs
+                        let pending = results.length
+
+                        results.map(row => {
+
+                            // calculations
+                            const previous_ep = row.ep
+                            const previous_gp = row.gp
+
+                            let new_ep = previous_ep
+                            let new_gp = previous_gp
+
+                            const ep_decay_amount = new_ep * DECAY_RATE * -1
+                            const gp_decay_amount = new_gp * DECAY_RATE * -1
+
+                            if (ep_amount) {
+                                new_ep = Number.parseFloat(previous_ep) + Number.parseFloat(ep_decay_amount)
+
+                                if (new_ep < 0) {
+                                    new_ep = 0
+                                }
+                            }
+                            if (gp_amount) {
+                                new_gp = Number.parseFloat(previous_gp) + Number.parseFloat(gp_decay_amount)
+    
+                                if (new_gp < MINIMUM_GP) {
+                                    new_gp = MINIMUM_GP
+                                }
+                            }
+
+                            // update epgp and record transaction
+                            connection.execute('UPDATE pug_epgp SET ep = ?, gp = ? WHERE id = ?', [new_ep, new_gp, row.id], (err, results, fieldS) => {
+                                if (err) {
+                                    console.error(err)
+                                    res.status(500).send('Server error')
+                                } else {
+                                    connection.execute('INSERT INTO pug_epgp_transactions (pug_id, ep_amount, gp_amount, note, previous_ep, previous_gp) VALUES (?, ?, ?, "Decay applied.",?, ?)', [row.id, ep_decay_amount, gp_decay_amount, previous_ep, previous_gp], (err, results, fields) => {
+                                        if (err) {
+                                            console.error(err)
+                                            res.status(500).send('Server error')
+                                        } else {
+                                            if (0 === --pending) {
+                                                res.status(200).send('Success')
+                                            }
+                                        }
+                                    })
+                                }
+                            })
+
+                        })
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.status(400).send('Invalid token')
+        })
+    }
 }
 
 const addCharacter = (req, res, connnection) => {
+    
+    // validate parameters
+    const { jwt, name } = req.body
 
+    if (typeof jwt === 'undefined' || typeof name === 'undefined') {
+        res.status(400).send('Bad request')
+    } else {
+
+        // verify jwt
+        JWT.verify(jwt)
+        .then(jwt_data => {
+
+            // confirm officer rank
+            if (!jwt_data.body.is_officer) {
+                res.status(403).send('Forbidden')
+            } else {
+
+                // add character
+                connection.execute('INSERT INTO pug_epgp (name) VALUES (?)', [name], (err, results, fields) => {
+                    if (err) {
+                        console.error(err)
+                        res.status(500).send('Server error')
+                    } else {
+                        res.status(200).send('Success')
+                    }
+                })
+            }   
+        })
+        .catch(err => {
+            res.status(400).send('Invalid token')
+        })
+    }
 }
 
 const deleteCharacter = (req, res, connection) => {
 
+    // validate parameters
+    const { jwt, id } = req.body
+
+    if (typeof jwt === 'undefined' || typeof id === 'undefined') {
+        res.status(400).send('Bad request')
+    } else {
+
+        // verify jwt
+        JWT.verify(jwt)
+        .then(jwt_data => {
+
+            // confirm officer rank
+            if (!jwt_data.body.is_officer) {
+                res.status(403).send('Forbidden')
+            } else {
+
+                // delete character
+                connection.execute('DELETE FROM pug_epgp WHERE id = ?', [id], (err, results, fields) => {
+                    if (err) {
+                        console.error(err)
+                        res.status(500).send('Server error')
+                    } else {
+                        res.status(200).send('Success')
+                    }
+                })
+            }   
+        })
+        .catch(err => {
+            res.status(400).send('Invalid token')
+        })
+    }
 }
 
 const activateCharacter = (req, res, connection) => {
 
+    // validate parameters
+    const { jwt, id } = req.body
+
+    if (typeof jwt === 'undefined' || typeof id === 'undefined') {
+        res.status(400).send('Bad request')
+    } else {
+
+        // verify jwt
+        JWT.verify(jwt)
+        .then(jwt_data => {
+
+            // confirm officer rank
+            if (!jwt_data.body.is_officer) {
+                res.status(403).send('Forbidden')
+            } else {
+
+                // update character
+                connection.execute('UPDATE pug_epgp SET active = TRUE WHERE id = ?', [id], (err, results, fields) => {
+                    if (err) {
+                        console.error(err)
+                        res.status(500).send('Server error')
+                    } else {
+                        res.status(200).send('Success')
+                    }
+                })
+            }   
+        })
+        .catch(err => {
+            res.status(400).send('Invalid token')
+        })
+    }
 }
 
 const deactivateCharacter = (req, res, connection) => {
 
+    // validate parameters
+    const { jwt, id } = req.body
+
+    if (typeof jwt === 'undefined' || typeof id === 'undefined') {
+        res.status(400).send('Bad request')
+    } else {
+
+        // verify jwt
+        JWT.verify(jwt)
+        .then(jwt_data => {
+
+            // confirm officer rank
+            if (!jwt_data.body.is_officer) {
+                res.status(403).send('Forbidden')
+            } else {
+
+                // update character
+                connection.execute('UPDATE pug_epgp SET active = FALSE WHERE id = ?', [id], (err, results, fields) => {
+                    if (err) {
+                        console.error(err)
+                        res.status(500).send('Server error')
+                    } else {
+                        res.status(200).send('Success')
+                    }
+                })
+            }   
+        })
+        .catch(err => {
+            res.status(400).send('Invalid token')
+        })
+    }
 }
 
 module.exports = {
