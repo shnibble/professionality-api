2733f859723a385240ef661edd859a8e95c387fa
Joshua Patterson <joshualpatterson@gmail.com>
2020-10-07 13:43:19 -0400

    Added attendance score to roster/members endpoint.

--- controllers/roster.js
+++ controllers/roster.js
@@ -22,23 +22,31 @@ const getPugs = (req, res, connection) => {
 }
 
 const getMembers = (req, res, connection) => {
-    connection.query('SELECT * FROM `users` WHERE `member` = TRUE AND `officer` = FALSE ORDER BY `nickname`', (err, results, fields) => {
+    connection.query('SELECT id FROM events WHERE primary_raid = TRUE ORDER BY start LIMIT 5', (err, results, fieldS) => {
         if (err) {
-            console.error(err)
             res.status(500).send('Server error')
         } else {
-            let final_results = []
-            let pending = results.length
-
-            results.map(row => {
-                connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
-                    row.characters = result
-                    final_results.push(row)
-
-                    if (0 === --pending) {
-                        res.status(200).json(final_results)
-                    }
-                })
+            const primary_raids = results.join()
+            
+            connection.execute('SELECT u.*, (SELECT COUNT(*) FROM attendance WHERE discord_user_id = u.discord_user_id AND event_id IN (?)) AS attendance FROM users u WHERE u.member = TRUE AND u.officer = FALSE ORDER BY u.nickname', [primary_raids], (err, results, fields) => {
+                if (err) {
+                    console.error(err)
+                    res.status(500).send('Server error')
+                } else {
+                    let final_results = []
+                    let pending = results.length
+        
+                    results.map(row => {
+                        connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
+                            row.characters = result
+                            final_results.push(row)
+        
+                            if (0 === --pending) {
+                                res.status(200).json(final_results)
+                            }
+                        })
+                    })
+                }
             })
         }
     })
