9ac9e4f5e4a8ad7059fb44caefcc826f14ceb944
Joshua Patterson <joshualpatterson@gmail.com>
2020-10-07 13:58:22 -0400

    Added attendance rating to all roster endpoints.

--- controllers/roster.js
+++ controllers/roster.js
@@ -1,21 +1,30 @@
 const getPugs = (req, res, connection) => {
-    connection.query('SELECT * FROM `users` WHERE `member` = FALSE AND `officer` = FALSE ORDER BY `nickname`', (err, results, fields) => {
+    connection.query('SELECT id FROM events WHERE primary_raid = TRUE ORDER BY start DESC LIMIT 5', (err, results, fieldS) => {
         if (err) {
-            console.error(err)
             res.status(500).send('Server error')
         } else {
-            let final_results = []
-            let pending = results.length
-            
-            results.map(row => {
-                connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
-                    row.characters = result
-                    final_results.push(row)
+            const primary_raids_array = results.map(row => row.id)
+            const primary_raids = primary_raids_array.join(',')
 
-                    if (0 === --pending) {
-                        res.status(200).json(final_results)
-                    }
-                })
+            connection.query(`SELECT u.*, (SELECT COUNT(*) FROM attendance WHERE discord_user_id = u.discord_user_id AND event_id IN (${primary_raids})) AS attendance FROM users u WHERE u.member = FALSE AND u.officer = FALSE ORDER BY u.nickname`, (err, results, fields) => {
+                if (err) {
+                    console.error(err)
+                    res.status(500).send('Server error')
+                } else {
+                    let final_results = []
+                    let pending = results.length
+                    
+                    results.map(row => {
+                        connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
+                            row.characters = result
+                            final_results.push(row)
+
+                            if (0 === --pending) {
+                                res.status(200).json(final_results)
+                            }
+                        })
+                    })
+                }
             })
         }
     })
@@ -28,10 +37,7 @@ const getMembers = (req, res, connection) => {
         } else {
             const primary_raids_array = results.map(row => row.id)
             const primary_raids = primary_raids_array.join(',')
-            console.log('primary_raids = ', primary_raids)
-            const sql = `SELECT u.*, (SELECT COUNT(*) FROM attendance WHERE discord_user_id = u.discord_user_id AND event_id IN (${primary_raids})) AS attendance FROM users u WHERE u.member = TRUE AND u.officer = FALSE ORDER BY u.nickname`
-            console.log('sql = ', sql)
-            connection.query(sql, (err, results, fields) => {
+            connection.query(`SELECT u.*, (SELECT COUNT(*) FROM attendance WHERE discord_user_id = u.discord_user_id AND event_id IN (${primary_raids})) AS attendance FROM users u WHERE u.member = TRUE AND u.officer = FALSE ORDER BY u.nickname`, (err, results, fields) => {
                 if (err) {
                     console.error(err)
                     res.status(500).send('Server error')
@@ -56,39 +62,47 @@ const getMembers = (req, res, connection) => {
 }
 
 const getOfficers = (req, res, connection) => {
-    connection.query('SELECT * FROM `users` WHERE `member` = TRUE AND `officer` = TRUE ORDER BY `nickname`', (err, results, fields) => {
+    connection.query('SELECT id FROM events WHERE primary_raid = TRUE ORDER BY start DESC LIMIT 5', (err, results, fieldS) => {
         if (err) {
-            console.error(err)
             res.status(500).send('Server error')
         } else {
-            let final_results = []
-            let pending = results.length
+            const primary_raids_array = results.map(row => row.id)
+            const primary_raids = primary_raids_array.join(',')
+            connection.query(`SELECT u.*, (SELECT COUNT(*) FROM attendance WHERE discord_user_id = u.discord_user_id AND event_id IN (${primary_raids})) AS attendance FROM users u WHERE u.member = TRUE AND u.officer = TRUE ORDER BY u.nickname`, (err, results, fields) => {
+                if (err) {
+                    console.error(err)
+                    res.status(500).send('Server error')
+                } else {
+                    let final_results = []
+                    let pending = results.length
 
-            if (pending === 0) {
-                res.status(200).json(final_results)
-            }
+                    if (pending === 0) {
+                        res.status(200).json(final_results)
+                    }
 
-            results.map(row => {
-                connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
-                    
-                    row.characters = result
-                    
-                    connection.execute(`
-                            SELECT uor.id, oroles.name
-                            FROM user_officer_roles uor
-                                INNER JOIN officer_roles oroles
-                                ON oroles.id = uor.officer_role_id
-                            WHERE uor.discord_user_id = ?
-                        `, [row.discord_user_id], (err, result, fields) => {
-                        
-                            row.officer_roles = result
-                            final_results.push(row)
+                    results.map(row => {
+                        connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
+                            
+                            row.characters = result
+                            
+                            connection.execute(`
+                                    SELECT uor.id, oroles.name
+                                    FROM user_officer_roles uor
+                                        INNER JOIN officer_roles oroles
+                                        ON oroles.id = uor.officer_role_id
+                                    WHERE uor.discord_user_id = ?
+                                `, [row.discord_user_id], (err, result, fields) => {
+                                
+                                    row.officer_roles = result
+                                    final_results.push(row)
 
-                            if (0 === --pending) {
-                                res.status(200).json(final_results)
-                            }
+                                    if (0 === --pending) {
+                                        res.status(200).json(final_results)
+                                    }
+                            })
+                        })
                     })
-                })
+                }
             })
         }
     })
