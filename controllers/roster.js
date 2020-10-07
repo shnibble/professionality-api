5b1a3af9ca12749a97615a0843065cabebb2469c
Joshua Patterson <joshualpatterson@gmail.com>
2020-10-07 12:03:57 -0400

    Added officer roles to roster.

--- controllers/roster.js
+++ controllers/roster.js
@@ -53,14 +53,26 @@ const getOfficers = (req, res, connection) => {
             let final_results = []
             let pending = results.length
 
+            if (!pending) {
+                res.status(200).json(final_results)
+            }
+
             results.map(row => {
                 connection.execute('SELECT * FROM `characters` WHERE `discord_user_id` = ? AND `enabled` = TRUE', [row.discord_user_id], (err, result, fields) => {
                     row.characters = result
-                    final_results.push(row)
-
-                    if (0 === --pending) {
-                        res.status(200).json(final_results)
-                    }
+                    connection.execute(`
+                            SELECT uor.id, or.name
+                            FROM user_officer_roles uor
+                                INNER JOIN officer_roles or
+                                ON or.id = uor.officer_role_id
+                            WHERE uor.discord_user_id = ?
+                        `, [row.discord_user_id], (err, result, fields) => {
+                        row.officer_roles = result
+                        final_results.push(row)
+                        if (0 === --pending) {
+                            res.status(200).json(final_results)
+                        }
+                    })
                 })
             })
         }
